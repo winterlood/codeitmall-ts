7056c023ecb3579cce4b826decebccc1faba10b8
winterlood <king199777@gmail.com>
2023-06-05 14:04:22 +0900

    타입스크립트 관련 패키지 추가

--- next.config.js
+++ next.config.js
@@ -4,22 +4,22 @@ const nextConfig = {
   async redirects() {
     return [
       {
-        source: '/products/:id',
-        destination: '/items/:id',
+        source: "/products/:id",
+        destination: "/items/:id",
         permanent: true,
       },
-    ]
+    ];
   },
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'learn-codeit-kr-static.s3.ap-northeast-2.amazonaws.com',
-        port: '',
-        pathname: '/codeitmall/**',
+        protocol: "https",
+        hostname: "learn-codeit-kr-static.s3.ap-northeast-2.amazonaws.com",
+        port: "",
+        pathname: "/codeitmall/**",
       },
     ],
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
