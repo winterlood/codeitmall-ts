d0594828e7094a6fbe562728c6f055e8f48415fc
winterlood <king199777@gmail.com>
2023-06-06 15:38:56 +0900

    app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,8 +3,12 @@ import Header from "@/components/Header";
 import Head from "next/head";
 import { ThemeProvider } from "@/lib/ThemeContext";
 import "@/styles/global.css";
+import { AppProps } from "next/app";
 
-export default function App({ Component, pageProps }) {
+export default function App({
+  Component,
+  pageProps,
+}: AppProps) {
   return (
     <>
       <Head>
